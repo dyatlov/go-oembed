a6b456a6eba382cc80275fb76632d9d4c3c6404a
Vitaly Dyatlov <md.xytop@gmail.com>
2015-12-21 20:48:32 +0200

    added locale and changed the way we fetch oembed info from sites

--- oembed/info.go
+++ oembed/info.go
@@ -4,26 +4,27 @@ import (
 	"encoding/json"
 	"io"
 	"io/ioutil"
-	"strconv"
+
+	"github.com/jeffail/gabs"
 )
 
 // Info returns information for embedding website
 type Info struct {
-	Status          int         `json:"-"`
-	Type            string      `json:"type"`
-	URL             string      `json:"url"`
-	ProviderURL     string      `json:"provider_url"`
-	ProviderName    string      `json:"provider_name"`
-	Title           string      `json:"title"`
-	Description     string      `json:"description"`
-	Width           json.Number `json:"width"`
-	Height          json.Number `json:"height"`
-	ThumbnailURL    string      `json:"thumbnail_url"`
-	ThumbnailWidth  json.Number `json:"thumbnail_width"`
-	ThumbnailHeight json.Number `json:"thumbnail_height"`
-	AuthorName      string      `json:"author_name"`
-	AuthorURL       string      `json:"author_url"`
-	HTML            string      `json:"html"`
+	Status          int    `json:"-"`
+	Type            string `json:"type"`
+	URL             string `json:"url"`
+	ProviderURL     string `json:"provider_url"`
+	ProviderName    string `json:"provider_name"`
+	Title           string `json:"title"`
+	Description     string `json:"description"`
+	Width           uint64 `json:"width"`
+	Height          uint64 `json:"height"`
+	ThumbnailURL    string `json:"thumbnail_url"`
+	ThumbnailWidth  uint64 `json:"thumbnail_width"`
+	ThumbnailHeight uint64 `json:"thumbnail_height"`
+	AuthorName      string `json:"author_name"`
+	AuthorURL       string `json:"author_url"`
+	HTML            string `json:"html"`
 }
 
 // NewInfo creater new instance of oembed.Info
@@ -39,24 +40,74 @@ func (info *Info) FillFromJSON(r io.Reader) error {
 		return err
 	}
 
-	err = json.Unmarshal(data, &info)
+	// We are not using standard json parsing into struct mechanism because it sucks in real life..
+	// when you expect a string some sites will return string, some will return null but some will return false
+	// when you expect an integer, some will return integer, some will return string but some will return null or false..
+	jsonParsed, err := gabs.ParseJSON(data)
 
 	if err != nil {
 		return err
 	}
 
-	var t int64
-	t, _ = strconv.ParseInt(info.Width.String(), 10, 64)
-	info.Width = json.Number(strconv.FormatInt(t, 10))
+	var strVal string
+	var intVal uint64
+	var ok bool
 
-	t, _ = strconv.ParseInt(info.Height.String(), 10, 64)
-	info.Height = json.Number(strconv.FormatInt(t, 10))
+	if strVal, ok = jsonParsed.Path("type").Data().(string); ok {
+		info.Type = strVal
+	}
 
-	t, _ = strconv.ParseInt(info.ThumbnailWidth.String(), 10, 64)
-	info.ThumbnailWidth = json.Number(strconv.FormatInt(t, 10))
+	if strVal, ok = jsonParsed.Path("url").Data().(string); ok {
+		info.URL = strVal
+	}
 
-	t, _ = strconv.ParseInt(info.ThumbnailHeight.String(), 10, 64)
-	info.ThumbnailHeight = json.Number(strconv.FormatInt(t, 10))
+	if strVal, ok = jsonParsed.Path("provider_url").Data().(string); ok {
+		info.ProviderURL = strVal
+	}
+
+	if strVal, ok = jsonParsed.Path("provider_name").Data().(string); ok {
+		info.ProviderName = strVal
+	}
+
+	if strVal, ok = jsonParsed.Path("title").Data().(string); ok {
+		info.Title = strVal
+	}
+
+	if strVal, ok = jsonParsed.Path("description").Data().(string); ok {
+		info.Description = strVal
+	}
+
+	if strVal, ok = jsonParsed.Path("thumbnail_url").Data().(string); ok {
+		info.ThumbnailURL = strVal
+	}
+
+	if strVal, ok = jsonParsed.Path("author_name").Data().(string); ok {
+		info.AuthorName = strVal
+	}
+
+	if strVal, ok = jsonParsed.Path("author_url").Data().(string); ok {
+		info.AuthorURL = strVal
+	}
+
+	if strVal, ok = jsonParsed.Path("html").Data().(string); ok {
+		info.HTML = strVal
+	}
+
+	if intVal, ok = jsonParsed.Path("width").Data().(uint64); ok {
+		info.Width = intVal
+	}
+
+	if intVal, ok = jsonParsed.Path("height").Data().(uint64); ok {
+		info.Height = intVal
+	}
+
+	if intVal, ok = jsonParsed.Path("thumbnail_width").Data().(uint64); ok {
+		info.ThumbnailWidth = intVal
+	}
+
+	if intVal, ok = jsonParsed.Path("thumbnail_height").Data().(uint64); ok {
+		info.ThumbnailHeight = intVal
+	}
 
 	return nil
 }
